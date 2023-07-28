e92a135dbf46a106e3f10fb306dd18f20e318f78
Crowdbotics <team@crowdbotics.com>
2023-07-28 21:00:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "small-water-42492",
+  url: "https://small-water-42492.botics.co",
+  api: "https://small-water-42492.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
